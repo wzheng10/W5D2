24f4b5188eea1f629c71b21adbed8b32b61ef8ab
Syldys <syldysnya@gmail.com>
2021-03-23 15:36:48 -0400

    Finish method 1

--- sql_zoo/lib/02_select_countries.rb
+++ sql_zoo/lib/02_select_countries.rb
@@ -25,6 +25,7 @@ def large_countries
   # Show the names of the countries that have a population of at least
   # 200 million. 200 million is 200,000,000 (eight zeros).
   execute(<<-SQL)
+  
   SQL
 end
 
